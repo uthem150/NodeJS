c0ec62671e56f938a0a93d55d19bb656c60822e7
uthem150 <uthem150@gmail.com>
2024-03-02 16:14:12 +0900

    express 설치

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var figlet = require("figlet");
+const express = require("express");
+const app = express();
 
-figlet("Kim Do Hun", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
+
+app.listen(3000);
